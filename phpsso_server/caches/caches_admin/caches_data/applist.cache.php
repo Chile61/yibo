d1b77bbf29ed071609d029ebc92a3603d4229e99
Rex <rexlin0624@gmail.com>
2018-12-12 11:04:51 +0800

    el

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://www.lldz.local/',
+    'authkey' => 'XBb5XSW60E1LgaR2wDiHcMwwlo5YTSbq',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
